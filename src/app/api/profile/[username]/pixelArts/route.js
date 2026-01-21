4705c7b099e34fff5af86fc8e2385809253969db
Lokesh Bisht <lokeshbisht009@gmail.com>
2026-01-21 21:05:52 +0530

    bug fix

--- src/app/api/profile/[username]/pixelArts/route.js
+++ src/app/api/profile/[username]/pixelArts/route.js
@@ -1,4 +1,5 @@
 import { getCurrentProfile } from "@/lib/getCurrentProfile";
+import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET(req, { params }) {
